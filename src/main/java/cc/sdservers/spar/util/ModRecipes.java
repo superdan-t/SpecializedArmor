102c99a94e195dd4c30ccc1d64d1b9dc0166b5f9
Dan T <superdan.tier@gmail.com>
2019-08-15 00:23:38 -0400

    Added smelting recipes

---
+++ src/main/java/cc/sdservers/spar/util/ModRecipes.java
@@ -0,0 +1,28 @@
+package cc.sdservers.spar.util;
+
+import cc.sdservers.spar.block.ModBlocks;
+import cc.sdservers.spar.item.ModItems;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModRecipes {
+	
+	public static void init() {
+		
+		//Ores
+		GameRegistry.addSmelting(ModBlocks.ALUMINUM_ORE, new ItemStack(ModItems.ALUMINUM_INGOT), 0.5F);
+		GameRegistry.addSmelting(ModBlocks.COPPER_ORE, new ItemStack(ModItems.COPPER_INGOT), 0.8F);
+		GameRegistry.addSmelting(ModBlocks.LEAD_ORE, new ItemStack(ModItems.LEAD_INGOT), 0.9F);
+		GameRegistry.addSmelting(ModBlocks.MOISSANITE_ORE, new ItemStack(ModItems.MOISSANITE), 3.0F);
+		GameRegistry.addSmelting(ModBlocks.TANTALUM_ORE, new ItemStack(ModItems.TANTALUM_INGOT), 0.7F);
+		GameRegistry.addSmelting(ModBlocks.TIN_ORE, new ItemStack(ModItems.TIN_INGOT), 0.7F);
+		GameRegistry.addSmelting(ModBlocks.TITANIUM_ORE, new ItemStack(ModItems.TITANIUM_INGOT), 1.0F);
+		GameRegistry.addSmelting(ModBlocks.ZIRCON_ORE, new ItemStack(ModItems.ZIRCON), 1.8F);
+		
+		//Materials
+		GameRegistry.addSmelting(ModItems.KAOLINITE_BALL, new ItemStack(ModItems.CERAMIC_BRICK), 0.3F);
+		GameRegistry.addSmelting(ModBlocks.KAOLINITE_BLOCK, new ItemStack(ModBlocks.CERAMIC_BLOCK), 0.35F);
+		
+	}
+
+}
