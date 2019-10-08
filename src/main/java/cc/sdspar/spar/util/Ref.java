7b6ad88b5b3a686b2e8a7d3b9407493e4c701625
Dan T <cat101126@gmail.com>
2019-10-08 01:01:57 -0400

    Fixed incorrect class name

--- src/main/java/cc/sdspar/spar/util/Ref.java
+++ src/main/java/cc/sdspar/spar/util/Ref.java
@@ -6,8 +6,8 @@ public class Ref {
 	public static final String NAME = "Superdan_t's Specialized Armor";
 	public static final String VERSION = "1.0.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "cc.sdservers.spar.main.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = "cc.sdservers.spar.main.CommonProxy";
+	public static final String CLIENT_PROXY_CLASS = "cc.sdspar.spar.main.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "cc.sdspar.spar.main.CommonProxy";
 	
 
 }
