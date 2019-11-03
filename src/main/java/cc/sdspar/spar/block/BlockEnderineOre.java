65e2f74f2113eb84bdcc9183a27d58df54e3ff2e
Dan T <cat101126@gmail.com>
2019-11-03 18:50:18 -0500

    Fix enderine blast resistance/hardness

---
+++ src/main/java/cc/sdspar/spar/block/BlockEnderineOre.java
@@ -0,0 +1,12 @@
+package cc.sdspar.spar.block;
+
+public class BlockEnderineOre extends BlockOre {
+
+	public BlockEnderineOre(String name) {
+		super(name, 3);
+		super.setResistance(2000.0F);
+		super.setHardness(30.0F);
+		
+	}
+
+}
