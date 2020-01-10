219044faaba1eccc5280ec0f7b394dde6ed8b8a2
Daniel Tierney <superdan.tier@gmail.com>
2020-01-10 01:50:50 -0500

    Better utilized proxies

--- src/main/java/cc/sdspar/spar/main/CommonProxy.java
+++ src/main/java/cc/sdspar/spar/main/CommonProxy.java
@@ -1,9 +1,28 @@
 package cc.sdspar.spar.main;
 
+import cc.sdspar.spar.inventory.handler.GuiHandler;
+import cc.sdspar.spar.util.ModRecipes;
+import cc.sdspar.spar.util.handler.RegistryHandler;
+import cc.sdspar.spar.world.ModWorldGen;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 
 	public void registerItemRenderer(Item item, int meta, String id) {}
+	
+    public void preInit(FMLPreInitializationEvent e) {
+    	GameRegistry.registerWorldGenerator(new ModWorldGen(), 0);
+    }
+	
+	public void init(FMLInitializationEvent e) {
+    	ModRecipes.init();
+    	RegistryHandler.registerOres();
+    	NetworkRegistry.INSTANCE.registerGuiHandler(Main.instance, new GuiHandler());
+	}
 	
 }
--- src/main/java/cc/sdspar/spar/main/Main.java
+++ src/main/java/cc/sdspar/spar/main/Main.java
@@ -1,11 +1,7 @@
 package cc.sdspar.spar.main;
 
-import cc.sdspar.spar.inventory.handler.GuiHandler;
 import cc.sdspar.spar.tabs.MaterialsTab;
-import cc.sdspar.spar.util.ModRecipes;
 import cc.sdspar.spar.util.Ref;
-import cc.sdspar.spar.util.handler.RegistryHandler;
-import cc.sdspar.spar.world.ModWorldGen;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -14,8 +10,6 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = Ref.MOD_ID, name = Ref.NAME, version = Ref.VERSION)
 public class Main {
@@ -30,14 +24,12 @@ public class Main {
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent e) {
-    	GameRegistry.registerWorldGenerator(new ModWorldGen(), 0);
+    	proxy.preInit(e);
     }
 
     @EventHandler
     public static void init(FMLInitializationEvent e) {
-    	ModRecipes.init();
-    	RegistryHandler.registerOres();
-    	NetworkRegistry.INSTANCE.registerGuiHandler(Main.instance, new GuiHandler());
+    	proxy.init(e);
     }
 
     @EventHandler
