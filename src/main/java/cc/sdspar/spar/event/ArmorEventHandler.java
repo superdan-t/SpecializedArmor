317df98eb1e34c563936306e940f90871a2333b8
Daniel Tierney <superdan.tier@gmail.com>
2020-02-02 23:34:50 -0500

    Removed annoying demo events

--- src/main/java/cc/sdspar/spar/event/ArmorEventHandler.java
+++ src/main/java/cc/sdspar/spar/event/ArmorEventHandler.java
@@ -9,22 +9,20 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber
 public class ArmorEventHandler {
-	
+/*	
 	@SubscribeEvent
 	public void playerDamaged(LivingDamageEvent event) {
-		System.out.println("An entity was damaged");
+		
 	}
 	
 	@SubscribeEvent
 	public void playerHurt(LivingHurtEvent e) {
-		if (e.getEntity() instanceof EntityPlayer) {
-			e.setAmount(0.01F);
-		}
+
 	}
 	
 	@SubscribeEvent
 	public void playerKnockback(LivingKnockBackEvent e) {
 		//e.setStrength(30000.0F);
 	}
-
+*/
 }
