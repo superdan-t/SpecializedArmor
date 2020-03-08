1b97729cec96539c6d8632d118ac1161d9c06aab
Dan T <superdan.tier@gmail.com>
2020-03-07 20:19:38 -0500

    Removed unnecessary event handlers

--- src/main/java/cc/sdspar/spar/event/StaticEventHandler.java
+++ src/main/java/cc/sdspar/spar/event/StaticEventHandler.java
@@ -1,28 +1,14 @@
 package cc.sdspar.spar.event;
 
-import cc.sdspar.spar.main.Main;
 import cc.sdspar.spar.network.api.NetworkAPI;
-import cc.sdspar.spar.network.api.NetworkAPIUtils;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientDisconnectionFromServerEvent;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent.ServerConnectionFromClientEvent;
 
 public class StaticEventHandler {
 
 	@SubscribeEvent
 	public static void playerConnects(ClientConnectedToServerEvent e) {
 		NetworkAPI.startClientAPI();
-	}
-	
-	@SubscribeEvent
-	public static void playerConnectsToServer(ServerConnectionFromClientEvent e) {
-		NetworkAPIUtils.notifyUser("I am the SERVER");
-	}
-	
-	@SubscribeEvent
-	public static void playerLeaves(ClientDisconnectionFromServerEvent e) {
-		
 	}
 	
 }
